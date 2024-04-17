3ae91d779321fb416216d9e04626bafbdf69a820
shahn <shahnawaz.alikhail@hotmail.com>
2024-04-17 11:32:04 -0700

    finsih log in and sign up activities

--- app/src/main/java/com/example/project2/LoginActivity.java
+++ app/src/main/java/com/example/project2/LoginActivity.java
@@ -1,4 +1,100 @@
-package com.example.project2;
+package com.example.floralhaven;
 
-public class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.floralhaven.dao.UsersDAO;
+import com.example.floralhaven.database.AppDatabase;
+import com.example.floralhaven.entities.Users;
+import com.example.floralhaven.utilities.CommonMethods;
+
+public class LoginActivity extends AppCompatActivity {
+
+    SharedPreferences appSharedPreferences;
+
+    private EditText editTextUsername, editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewCreateAccount;
+
+    private UsersDAO usersDAO;
+
+    private static final String SHARED_PREF_NAME = "app_shared_data";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_USER_ID = "user_id";
+    private static final String KEY_USER_EMAIL = "email";
+    private static final String KEY_USER_IS_ADMIN = "is_admin";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        appSharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        if(appSharedPreferences.getString(KEY_USERNAME, null) != null) {
+            Toast.makeText(this, "Already logged in", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(LoginActivity.this, com.example.floralhaven.HomeActivity.class));
+            finish();
+        }
+
+        AppDatabase appDatabase = AppDatabase.getDBInstance(this);
+        usersDAO = appDatabase.usersDAO();
+        validateDefaultRecords();
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textViewCreateAccount = findViewById(R.id.textViewCreateAccount);
+
+        buttonLogin.setOnClickListener(v -> { login(); });
+        textViewCreateAccount.setOnClickListener(v -> { startActivity(new Intent(LoginActivity.this, SignUpActivity.class)); });
+    }
+
+    private void validateDefaultRecords() {
+        if(usersDAO.getUserCount() == 0) {
+            usersDAO.insert(new Users("testuser1".toUpperCase(), "testuser1@gmail.com".toUpperCase(), CommonMethods.hashPassword("testuser1"), false));
+            usersDAO.insert(new Users("admin2".toUpperCase(), "admin2@gmail.com".toUpperCase(), CommonMethods.hashPassword("admin2"), true));
+        }
+    }
+
+    private void login() {
+        String username = editTextUsername.getText().toString().trim().toUpperCase();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Users users = usersDAO.getUserByUsername(username);
+        if (users == null) { users = usersDAO.getUserByEmail(username); }
+
+        if (users == null) {
+            Toast.makeText(this, "User does not exist", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!CommonMethods.hashPassword(password).equals(users.getPassword())) {
+            Toast.makeText(this, "Incorrect password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        SharedPreferences.Editor sharedEditor = appSharedPreferences.edit();
+        sharedEditor.putInt(KEY_USER_ID, users.getUserId());
+        sharedEditor.putString(KEY_USERNAME, users.getUsername());
+        sharedEditor.putString(KEY_USER_EMAIL, users.getEmail());
+        sharedEditor.putBoolean(KEY_USER_IS_ADMIN, users.getIsAdmin());
+        sharedEditor.apply();
+
+        Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
+
+        startActivity(new Intent(LoginActivity.this, com.example.floralhaven.HomeActivity.class));
+        finish();
+    }
 }
--- app/src/main/java/com/example/project2/signupActivity.java
+++ app/src/main/java/com/example/project2/signupActivity.java
@@ -1,4 +1,100 @@
-package com.example.project2;
+package com.example.floralhaven;
 
-public class signupActivity {
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.floralhaven.dao.UsersDAO;
+import com.example.floralhaven.database.AppDatabase;
+import com.example.floralhaven.entities.Users;
+import com.example.floralhaven.utilities.CommonMethods;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText editTextNewUsername, editTextNewEmail, editTextNewPassword, editTextConfirmPassword;
+    private Button buttonSignUp;
+
+    private UsersDAO usersDAO;
+
+    SharedPreferences appSharedPreferences;
+    private static final String SHARED_PREF_NAME = "app_shared_data";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_USER_ID = "user_id";
+    private static final String KEY_USER_EMAIL = "email";
+    private static final String KEY_USER_IS_ADMIN = "is_admin";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        appSharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+
+        AppDatabase appDatabase = AppDatabase.getDBInstance(this);
+        usersDAO = appDatabase.usersDAO();
+
+        editTextNewUsername = findViewById(R.id.editTextNewUsername);
+        editTextNewEmail = findViewById(R.id.editTextNewEmail);
+        editTextNewPassword = findViewById(R.id.editTextNewPassword);
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+
+        buttonSignUp.setOnClickListener(v -> signUp());
+    }
+
+    private void signUp() {
+        String newUsername = editTextNewUsername.getText().toString().trim().toUpperCase();
+        String newEmail = editTextNewEmail.getText().toString().trim().toUpperCase();
+        String newPassword = editTextNewPassword.getText().toString().trim();
+        String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+
+        if (newUsername.isEmpty() || newPassword.isEmpty() || confirmPassword.isEmpty()) {
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!newPassword.equals(confirmPassword)) {
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Users existingUser = usersDAO.getUserByUsername(newUsername);
+        if (existingUser != null) {
+            Toast.makeText(this, "Username already exists", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        existingUser = usersDAO.getUserByEmail(newEmail);
+        if (existingUser != null) {
+            Toast.makeText(this, "Email already exists", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Users newUser = new Users(newUsername, newEmail, CommonMethods.hashPassword(newPassword), false);
+        usersDAO.insert(newUser);
+        newUser = usersDAO.getUserByUsername(newUsername);
+
+        Toast.makeText(this, "Sign Up successful", Toast.LENGTH_SHORT).show();
+
+        SharedPreferences.Editor sharedEditor = appSharedPreferences.edit();
+        sharedEditor.putInt(KEY_USER_ID, newUser.getUserId());
+        sharedEditor.putString(KEY_USERNAME, newUser.getUsername());
+        sharedEditor.putString(KEY_USER_EMAIL, newUser.getEmail());
+        sharedEditor.putBoolean(KEY_USER_IS_ADMIN, newUser.getIsAdmin());
+        sharedEditor.apply();
+
+        startActivity(new Intent(SignUpActivity.this, com.example.floralhaven.HomeActivity.class));
+        finish();
+    }
+
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(this, com.example.floralhaven.LoginActivity.class));
+        finish();
+    }
 }
