4c4be16298e1a6b766aac15c09e869e8e75a9194
shahn <shahnawaz.alikhail@hotmail.com>
2024-04-17 18:25:59 -0700

    impelement Dao and also user class.

--- app/src/main/java/com/example/project2/dao/UsersDAO.java
+++ app/src/main/java/com/example/project2/dao/UsersDAO.java
@@ -1,4 +1,38 @@
-package com.example.project2.dao;
+package com.example.floralhaven.dao;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.floralhaven.entities.Users;
+import java.util.List;
+
+@Dao
 public interface UsersDAO {
+    @Insert
+    void insert(Users users);
+
+    @Query("SELECT * FROM users WHERE username = :username LIMIT 1")
+    Users getUserByUsername(String username);
+
+    @Query("SELECT * FROM users WHERE email = :email LIMIT 1")
+    Users getUserByEmail(String email);
+
+    @Query("SELECT * FROM users WHERE user_id = :user_id LIMIT 1")
+    Users getUserById(int user_id);
+
+    @Query("SELECT * FROM users WHERE is_admin = 1 ORDER BY username")
+    List<Users> getAdmins();
+
+    @Query("DELETE FROM users WHERE is_admin = 0")
+    void deleteUsers();
+
+    @Query("SELECT COUNT(*) FROM users")
+    int getUserCount();
+
+    @Query("SELECT * FROM users WHERE is_admin = 0 ORDER BY username")
+    List<Users> getUsers();
+
+    @Query("DELETE FROM users WHERE user_id = (:user_id)")
+    void deleteUserByID(int user_id);
 }
--- app/src/main/java/com/example/project2/entities/users.java
+++ app/src/main/java/com/example/project2/entities/users.java
@@ -1,4 +1,41 @@
-package com.example.project2.entities;
+package com.example.floralhaven.entities;
 
-public class users {
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class Users {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "user_id")
+    private int userId;
+
+    private String username;
+    private String email;
+    private String password;
+
+    @ColumnInfo(name = "is_admin")
+    private boolean isAdmin;
+
+    public Users(String username, String email, String password, boolean isAdmin) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public boolean getIsAdmin() { return isAdmin; }
+    public void setIsAdmin(boolean isAdmin) { isAdmin = isAdmin; }
 }
